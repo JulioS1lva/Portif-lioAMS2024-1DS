63cf515d9a6f3474ab962c21e752607e2c962791
Júlio César <166918259+JulioS1lva@users.noreply.github.com>
2024-09-18 05:46:41 -0700

    Todos os pares existentes entre os números 6 e 600

---
+++ Exercicio1.java
@@ -0,0 +1,10 @@
+package com.mycompany.exercicio1;
+public class Exercicio1 {
+    public static void main(String[] args) {
+        for(int i = 1+1; i < 200; i++){
+            if(i%2==0){
+            System.out.println(i);
+            }
+        }
+    }
+}

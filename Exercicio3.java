b43fd1bbc9172a57a9e7f1059c0d2185ebe559f4
Júlio César <166918259+JulioS1lva@users.noreply.github.com>
2024-09-18 05:48:39 -0700

    números existentes entre 6000 e 12000

---
+++ Exercicio3.java
@@ -0,0 +1,10 @@
+package com.mycompany.exercicio3;
+public class Exercicio3 {
+    public static void main(String[] args) {
+        int n = 0;
+        for(int i = 1250+1; i < 1500; i++){
+          n = n+1; 
+        }
+        System.out.println("Existem "+(n)+" numeros entre 1250 e 1500.");
+    }
+}
